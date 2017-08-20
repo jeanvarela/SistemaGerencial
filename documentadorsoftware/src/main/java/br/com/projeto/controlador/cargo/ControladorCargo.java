adecbf642dd4cd29c57326f8e1858d604d015ba3
jean <jean.varela1@gmail.com>
2017-08-19 23:43:08 -0300

    Criado um método para recuperar a lista de contatos

--- documentadorsoftware/src/main/java/br/com/projeto/controlador/cargo/ControladorCargo.java
+++ documentadorsoftware/src/main/java/br/com/projeto/controlador/cargo/ControladorCargo.java
@@ -1,5 +1,7 @@
 package br.com.projeto.controlador.cargo;
 
+import java.util.List;
+
 import br.com.projeto.modelo.entidade.cargo.Cargo;
 import br.com.projeto.modelo.repositorio.cargo.IRepositorioCargo;
 import br.com.projeto.modelo.repositorio.cargo.RepositorioCargo;
@@ -15,6 +17,11 @@ public class ControladorCargo implements IControladorCargo {
 	@Override
 	public long insereCargo(Cargo cargo) {
 	    return repositorioCargo.salvar(cargo); 
+	}
+
+	@Override
+	public List<Cargo> buscarListaCargo(String... propriedades) {
+		return repositorioCargo.listar(propriedades);
 	}
 
 }
--- documentadorsoftware/src/main/java/br/com/projeto/controlador/cargo/IControladorCargo.java
+++ documentadorsoftware/src/main/java/br/com/projeto/controlador/cargo/IControladorCargo.java
@@ -1,9 +1,12 @@
 package br.com.projeto.controlador.cargo;
 
+import java.util.List;
+
 import br.com.projeto.modelo.entidade.cargo.Cargo;
 
 public interface IControladorCargo {
 	
-	public long insereCargo(Cargo cargo);
-
+	public long  insereCargo(Cargo cargo);
+	
+	public List<Cargo> buscarListaCargo(String... propriedades);
 }
